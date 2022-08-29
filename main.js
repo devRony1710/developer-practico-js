9c59bb60be0da0c3e6656542aad03e00a84d8fc8
Rony Antolinez <rony1710.dev@gmail.com>
2022-08-29 13:03:15 -0400

    merge mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenuIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 const toggleDesktopMenu = () => {
   desktopMenu.classList.toggle("inactive")
 }
 
+const toggleMobileMenu = () => {
+  mobileMenu.classList.toggle("inactive")
+}
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenuIcon.addEventListener('click', toggleMobileMenu);
